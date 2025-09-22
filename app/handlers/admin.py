3ef5e5b9981d8885d839dd918a002d331427b32b
Данила Рябинин <ryabinin.danila355@mail.ru>
2025-09-22 20:36:58 +0300

    update prod admin

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -5,6 +5,7 @@ from aiogram.types import Message, CallbackQuery
 
 from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import joinedload
 
 from app.core.models import Order, Category, Product
 from app.keyboards.admin import edit_status_keyboard
@@ -22,6 +23,14 @@ class CreateNewProduct(StatesGroup):
     photo = State()
     description = State()
 
+
+class UpdateProduct(StatesGroup):
+    new_name = State()
+    catalog = State()
+    name = State()
+    price = State()
+    photo = State()
+    description = State()
 
 class OrderStatus(StatesGroup):
     order_id = State()
@@ -174,6 +183,7 @@ async def proccess_description(
 
     if not category:
         await message.answer('Такого каталога не нашлось')
+        return
 
     new_product = Product(
         name=data['name'],
@@ -185,7 +195,151 @@ async def proccess_description(
     session.add(new_product)
     await session.commit()
 
+    await state.clear()
+
     await message.answer(
         'Товар успешно добавлен',
         reply_markup=await main_kb(await check_admin(message, session))
     )
+
+
+@router.message(F.text == 'Изменение товаров')
+async def update_product(
+        message: Message,
+        session: AsyncSession,
+        state: FSMContext
+):
+    is_admin = await check_admin(message, session)
+
+    if is_admin:
+        await message.answer(
+            'Введите точное название товара,'
+            ' который хотите поменять:'
+        )
+        await state.set_state(UpdateProduct.name)
+
+
+@router.message(UpdateProduct.name)
+async def admin_update_product(
+        message: Message,
+        session: AsyncSession,
+        state: FSMContext
+):
+    await state.update_data(name=message.text.strip())
+
+    stmt = select(Product).options(joinedload(
+        Product.category
+    )).where(
+        Product.name == message.text.strip()
+    )
+    product = (await session.execute(stmt)).scalar_one_or_none()
+
+    if not product:
+        await message.answer('Такого продукта нету')
+        return
+
+    await message.answer(
+        f'Нынешняя информация:\n'
+        f'Название: {product.name}\n'
+        f'Описание: {product.description}\n'
+        f'Цена: {product.price}\n'
+        f'Фото: {product.photo}\n'
+        f'Категория: {product.category.name}\n\n'
+        f'Скинул информацию о товаре, чтобы лучше было копировать то, что не нужно менять\n\n'
+        f'НАПИШИТЕ НОВОЕ НАЗВАНИЕ ИЛИ ОСТАВЬТЕ СТАРОЕ:'
+    )
+    await state.set_state(UpdateProduct.new_name)
+
+
+@router.message(UpdateProduct.new_name)
+async def proccess_new_name_update(
+        message: Message,
+        session: AsyncSession,
+        state: FSMContext
+):
+    await state.update_data(new_name=message.text.strip())
+
+    stmt = select(Category)
+    categories = (await session.execute(stmt)).scalars().all()
+    text = 'Категории:\n'
+    for category in categories:
+        text += f'{category.name}\n'
+
+
+    await message.answer(text + '\n' + 'Введите нужную категорию, написав цифру:\n')
+    await state.set_state(UpdateProduct.catalog)
+
+
+@router.message(UpdateProduct.catalog)
+async def admin_proccess_catalog(
+        message: Message,
+        session: AsyncSession,
+        state: FSMContext
+):
+    await state.update_data(catalog=message.text.strip())
+
+    stmt = select(Category).where(
+        Category.name == message.text.strip()
+    )
+    category = (await session.execute(stmt)).scalar_one_or_none()
+
+    if not category:
+        await message.answer(
+            'Такой категории нету'
+        )
+        return
+
+    await message.answer('Введите описание:')
+    await state.set_state(UpdateProduct.description)
+
+
+@router.message(UpdateProduct.description)
+async def admin_proccess_description(
+        message: Message,
+        state: FSMContext
+):
+    await state.update_data(description=message.text.strip())
+    await message.answer('Введите цену:')
+    await state.set_state(UpdateProduct.price)
+
+
+@router.message(UpdateProduct.price)
+async def admin_proccess_price(
+        message: Message,
+        state: FSMContext
+):
+    await state.update_data(price=message.text.strip())
+    await message.answer('Введите ссылку на фото:')
+    await state.set_state(UpdateProduct.photo)
+
+
+@router.message(UpdateProduct.photo)
+async def admin_proccess_photo(
+        message: Message,
+        session: AsyncSession,
+        state: FSMContext
+):
+    await state.update_data(photo=message.text.strip())
+    data = await state.get_data()
+
+    stmt_cat = select(Category).where(
+        Category.name == data['catalog']
+    )
+    category = (await session.execute(stmt_cat)).scalar_one_or_none()
+
+    stmt_update_prod = update(Product).where(
+        Product.name == data['name']
+    ).values(
+        name=data['new_name'],
+        price=int(data['price']),
+        description=data['description'],
+        photo=data['photo'],
+        category_id=category.id
+    )
+    await session.execute(stmt_update_prod)
+    await session.commit()
+    await message.answer(
+        'Продукт обновлен',
+        reply_markup=await main_kb(await check_admin(message, session))
+    )
+    await state.clear()
